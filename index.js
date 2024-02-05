879780e3802f595a8ea0000171d1e93f5cdf27f2
A.X.E.L <108288065+7AXEL@users.noreply.github.com>
2024-02-05 02:44:45 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function open_side_nav() {
+	document.getElementById("open_btn").style.display="none";
+	document.getElementById("sidenav").style.display="inline-block"
+}
+function close_side_nav() {
+	document.getElementById("sidenav").style.display="none";
+	document.getElementById("open_btn").style.display="inline-block"
+}
+
